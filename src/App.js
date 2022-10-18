fb700ebfb10cfffd5b1298b55989917688183b60
mikohan <angara99@gmail.com>
2022-10-18 12:13:15 +0300

    Added test docker

--- src/App.js
+++ src/App.js
@@ -7,8 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          I am cool! <code>src/App.js</code> and save to reload.
-
+          I am very cool again! <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
